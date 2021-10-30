3d7777feaa170abdf8303101b3cd62000cb21c7e
陈阳 <1337968347@qq.com>
2021-10-30 23:50:52 +0800

    :Star: init

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import * as THREE from "three";
+
+let camera: THREE.Camera;
+let scene: THREE.Scene;
+let renderer: THREE.WebGLRenderer;
+let geometry: THREE.BoxGeometry;
+let material: THREE.Material;
+let mesh: THREE.Mesh;
+
+init();
+
+function init() {
+  camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    10
+  );
+  camera.position.z = 1;
+
+  scene = new THREE.Scene();
+
+  geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+  material = new THREE.MeshNormalMaterial();
+
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setAnimationLoop(animation);
+  document.body.appendChild(renderer.domElement);
+}
+
+function animation(time: number) {
+  mesh.rotation.x = time / 2000;
+  mesh.rotation.y = time / 1000;
+
+  renderer.render(scene, camera);
+}
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: false,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js$|jsx/,
+        exclude: /node_modules/,
+      },
+      {
+        loader: "ts-loader",
+        test: /\.ts/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.bundle.js",
+  },
+};
